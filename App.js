f264da34f817c956c26ba629b71903e57a8ca3ac
Gowthami0908 <gowthamianandthalari@gmail.com>
2025-03-21 10:51:41 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Dashboard from"./components/Dashboard";
+function App()
+{
+  return(
+    <div classname="App">
+    <header>
+      <h1>Social media analytics</h1>
+    </header>
+    <main>
+      <Dashboard  />
+    </main>
+    </div>
+  )
+}
